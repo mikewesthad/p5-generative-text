ed2be24b1356a74c155f356a2495303c379e4ed9
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-17 23:17:39 -0500

    Rebuild

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,274 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = BboxAlignedText;
+
+/**
+ * Creates a new BboxAlignedText object - a text object that can be drawn with
+ * anchor points based on a tight bounding box around the text.
+ * @constructor
+ * @param {object} font               p5.Font object
+ * @param {string} text               String to display
+ * @param {number} [fontSize=12]      Font size to use for string
+ * @param {object} [pInstance=window] Reference to p5 instance, leave blank if
+ *                                    sketch is global
+ * @example
+ * var font, bboxText;
+ * function preload() {
+ *     font = loadFont("./assets/Regular.ttf");
+ * }
+ * function setup() {
+ *     createCanvas(400, 600);
+ *     background(0);
+ *     
+ *     bboxText = new BboxAlignedText(font, "Hey!", 30);    
+ *     bboxText.setRotation(PI / 4);
+ *     bboxText.setAnchor(BboxAlignedText.ALIGN.CENTER, 
+ *                        BboxAlignedText.BASELINE.CENTER);
+ *     
+ *     fill("#00A8EA");
+ *     noStroke();
+ *     bboxText.draw(width / 2, height / 2, true);
+ * }
+ */
+function BboxAlignedText(font, text, fontSize, pInstance) {
+    this._font = font;
+    this._text = text;
+    this._fontSize = (fontSize !== undefined) ? fontSize : 12;
+    this.p = pInstance || window; // If instance is omitted, assume global scope
+    this._rotation = 0;
+    this._hAlign = BboxAlignedText.ALIGN.CENTER;
+    this._vAlign = BboxAlignedText.BASELINE.CENTER;
+    this._calculateMetrics(true);
+}
+
+/**
+ * Vertical alignment values
+ * @public
+ * @static
+ * @readonly
+ * @enum {string}
+ */
+BboxAlignedText.ALIGN = {
+    /** Draw from the left of the bbox */
+    LEFT: "left",
+    /** Draw from the center of the bbox */
+    CENTER: "center",
+    /** Draw from the right of the bbox */
+    RIGHT: "right"
+};
+
+/**
+ * Baseline alignment values
+ * @public
+ * @static
+ * @readonly
+ * @enum {string}
+ */
+BboxAlignedText.BASELINE = {
+    /** Draw from the top of the bbox */
+    BOX_TOP: "top",
+    /** Draw from the center of the bbox */
+    BOX_CENTER: "center",
+    /** Draw from the bottom of the bbox */
+    BOM_BOTTOM: "bottom",
+    /** 
+     * Draw from half the height of the font. Specifically the height is
+     * calculated as: ascent + descent.
+     */
+    FONT_CENTER: "font_center",
+    /** Draw from the the normal font baseline */
+    ALPHABETIC: "alphabetic"
+};
+
+/**
+ * Set current text
+ * @public
+ * @param {string} string Text string to display
+ */
+BboxAlignedText.prototype.setText = function(string) {
+    this._text = string;
+    this._calculateMetrics(false);
+};
+
+/**
+ * Set current text size
+ * @public
+ * @param {number} fontSize Text size
+ */
+BboxAlignedText.prototype.setTextSize = function(fontSize) {
+    this._fontSize = fontSize;
+    this._calculateMetrics(true);
+};
+
+/**
+ * Set rotation of text
+ * @public
+ * @param {number} angle Rotation in radians
+ */
+BboxAlignedText.prototype.setRotation = function(angle) {
+    this._rotation = angle;
+};
+
+/**
+ * Set anchor point for text (horizonal and vertical alignment) relative to
+ * bounding box
+ * @public
+ * @param {string} [hAlign=CENTER] Horizonal alignment
+ * @param {string} [vAlign=CENTER] Vertical baseline
+ */
+BboxAlignedText.prototype.setAnchor = function(hAlign, vAlign) {
+    this._hAlign = hAlign || BboxAlignedText.ALIGN.CENTER;
+    this._vAlign = vAlign || BboxAlignedText.BASELINE.CENTER;
+};
+
+/**
+ * Get the bounding box when the text is placed at the specified coordinates.
+ * Note: this is the unrotated bounding box!
+ * @param  {number} x X coordinate
+ * @param  {number} y Y coordinate
+ * @return {object}   Returns an object with properties: x, y, w, h
+ */
+BboxAlignedText.prototype.getBbox = function(x, y) {
+    var pos = this._calculateAlignedCoords(x, y);
+    return {
+        x: pos.x + this._boundsOffset.x,
+        y: pos.y + this._boundsOffset.y,
+        w: this.width,
+        h: this.height
+    };
+};
+
+/**
+ * Draws the text particle with the specified style parameters
+ * @public
+ * @param  {number}  x                  X coordinate of text anchor
+ * @param  {number}  y                  Y coordinate of text anchor
+ * @param  {boolean} [drawBounds=false] Flag for drawing bounding box
+ */
+BboxAlignedText.prototype.draw = function(x, y, drawBounds) {
+    drawBounds = drawBounds || false;
+    var pos = {
+        x: (x !== undefined) ? x : 0, 
+        y: (y !== undefined) ? y : 0
+    };
+
+    this.p.push();
+
+        if (this._rotation) {
+            pos = this._calculateRotatedCoords(pos.x, pos.y, this._rotation);
+            this.p.rotate(this._rotation);
+        }
+
+        pos = this._calculateAlignedCoords(pos.x, pos.y);
+
+        this.p.textAlign(this.p.LEFT, this.p.BASELINE);
+        this.p.textFont(this._font);
+        this.p.textSize(this._fontSize);
+        this.p.text(this._text, pos.x, pos.y);
+
+        if (drawBounds) {
+            this.p.stroke(200);
+            var boundsX = pos.x + this._boundsOffset.x;
+            var boundsY = pos.y + this._boundsOffset.y;
+            this.p.noFill();
+            this.p.rect(boundsX, boundsY, this.width, this.height);            
+        }
+
+    this.p.pop();
+};
+
+/**
+ * Project the coordinates (x, y) into a rotated coordinate system
+ * @private
+ * @param  {number} x     X coordinate (in unrotated space)
+ * @param  {number} y     Y coordinate (in unrotated space)
+ * @param  {number} angle Radians of rotation to apply
+ * @return {object}       Object with x & y properties
+ */
+BboxAlignedText.prototype._calculateRotatedCoords = function (x, y, angle) {  
+    var rx = Math.cos(angle) * x + Math.cos(Math.PI / 2 - angle) * y;
+    var ry = -Math.sin(angle) * x + Math.sin(Math.PI / 2 - angle) * y;
+    return {x: rx, y: ry};
+};
+
+/**
+ * Calculates draw coordinates for the text, aligning based on the bounding box.
+ * The text is eventually drawn with canvas alignment set to left & baseline, so
+ * this function takes a desired pos & alignment and returns the appropriate
+ * coordinates for the left & baseline.
+ * @private
+ * @param  {number} x      X coordinate
+ * @param  {number} y      Y coordinate
+ * @return {object}        Object with x & y properties
+ */
+BboxAlignedText.prototype._calculateAlignedCoords = function(x, y) {
+    var newX, newY;
+    switch (this._hAlign) {
+        case BboxAlignedText.ALIGN.LEFT:
+            newX = x;
+            break;
+        case BboxAlignedText.ALIGN.CENTER:
+            newX = x - this.halfWidth;
+            break;
+        case BboxAlignedText.ALIGN.RIGHT:
+            newX = x - this.width;
+            break;
+        default:
+            newX = x;
+            console.log("Unrecognized horizonal align:", this._hAlign);
+            break;
+    }
+    switch (this._vAlign) {
+        case BboxAlignedText.BASELINE.TOP:
+            newY = y - this._boundsOffset.y;
+            break;
+        case BboxAlignedText.BASELINE.CENTER:
+            newY = y + this._distBaseToMid;
+            break;
+        case BboxAlignedText.BASELINE.BOTTOM:
+            newY = y - this._distBaseToBottom;
+            break;
+        case BboxAlignedText.BASELINE.FONT_CENTER:
+            // Height is approximated as ascent + descent
+            newY = y - this._descent + (this._ascent + this._descent) / 2;
+            break;
+        case BboxAlignedText.BASELINE.ALPHABETIC:
+            newY = y;
+            break;
+        default:
+            newY = y;
+            console.log("Unrecognized vertical align:", this._vAlign);
+            break;
+    }
+    return {x: newX, y: newY};
+};
+
+
+/**
+ * Calculates bounding box and various metrics for the current text and font
+ * @private
+ */
+BboxAlignedText.prototype._calculateMetrics = function(shouldUpdateHeight) {  
+    // p5 0.5.0 has a bug - text bounds are clipped by (0, 0)
+    // Calculating bounds hack
+    var bounds = this._font.textBounds(this._text, 1000, 1000, this._fontSize);
+    bounds.x -= 1000;
+    bounds.y -= 1000;
+
+    if (shouldUpdateHeight) {
+        this._ascent = this._font._textAscent(this._fontSize);
+        this._descent = this._font._textDescent(this._fontSize);
+    }
+
+    // Use bounds to calculate font metrics
+    this.width = bounds.w;
+    this.height = bounds.h;
+    this.halfWidth = this.width / 2;
+    this.halfHeight = this.height / 2;
+    this._boundsOffset = {x: bounds.x, y: bounds.y};
+    this._distBaseToMid = Math.abs(bounds.y) - this.halfHeight;
+    this._distBaseToBottom = this.height - Math.abs(bounds.y);
+};
+},{}],2:[function(require,module,exports){
 module.exports = {
     NoiseGenerator1D: NoiseGenerator1D,
     NoiseGenerator2D: NoiseGenerator2D
@@ -108,26 +378,27 @@ NoiseGenerator2D.prototype.generate = function () {
         y: this.yNoise.generate()
     };
 };
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 require("./sketches/noisy-word.js")();
 require("./sketches/halftone-flashlight-word.js")();
-},{"./sketches/halftone-flashlight-word.js":3,"./sketches/noisy-word.js":4}],3:[function(require,module,exports){
+},{"./sketches/halftone-flashlight-word.js":4,"./sketches/noisy-word.js":5}],4:[function(require,module,exports){
 module.exports = startSketch;
 
 // Modules
 var dom = require("../utilities/dom-utilities.js");
 var Noise = require("../generators/noise-generators.js");
+var BboxText = require("p5-bbox-aligned-text");
 
 // Globals
-var p, font, circles;
+var p, font, circles, bboxText;
 var isFirstFrame = true;
 var isMouseOver = false;
 var canvasSize = {
     width: 400,
     height: 150
 };
-var text = "String";
-var fontSize = 130;
+var text = "halftone";
+var fontSize = 150;
 var fontPath = "./assets/fonts/leaguegothic-regular-webfont.ttf";
 
 function startSketch() { 
@@ -165,20 +436,26 @@ function setup() {
 
     // Draw the stationary text
     p.background(255);
-    p.textFont(font);
     p.textSize(fontSize);
-    p.textAlign(p.CENTER, p.CENTER);
+    bboxText = new BboxText(font, text, fontSize, p);
+    bboxText.setAnchor(BboxText.ALIGN.CENTER, BboxText.BASELINE.FONT_CENTER);
     p.noStroke();
     p.fill("#0A000A");    
-    p.text(text, p.width / 2, p.height / 2);
+    bboxText.draw(p.width / 2, p.height / 2);
 
-    // Loop over the pixels in the canvas to sample the word
+
+    // Loop over the pixels in the text's bounding box to sample the word
+    var bbox = bboxText.getBbox(p.width / 2, p.height / 2);
+    var startX = Math.floor(Math.max(bbox.x - 5, 0));
+    var endX = Math.ceil(Math.min(bbox.x + bbox.w + 5, p.width));
+    var startY = Math.floor(Math.max(bbox.y - 5, 0));
+    var endY = Math.ceil(Math.min(bbox.y + bbox.h + 5, p.height));
     var spacing = 5;
     p.loadPixels();
     p.pixelDensity(1);
     circles = [];
-    for (var y = 0; y < p.height; y += spacing) {
-        for (var x = 0; x < p.width; x += spacing) {            
+    for (var y = startY; y < endY; y += spacing) {
+        for (var x = startX; x < endX; x += spacing) {  
             var i = 4 * ((y * p.width) + x);
             var r = p.pixels[i];
             var g = p.pixels[i + 1];
@@ -229,12 +506,12 @@ function draw() {
         var circle = circles[i];
         var c = circle.color;
         var dist = p.dist(circle.x, circle.y, p.mouseX, p.mouseY);
-        var radius = p.map(dist, 0, 120, 1, 10);
+        var radius = p.map(dist, 0, 150, 1, 10);
         p.fill(c);
         p.ellipse(circle.x, circle.y, radius, radius);
     }
 }
-},{"../generators/noise-generators.js":1,"../utilities/dom-utilities.js":5}],4:[function(require,module,exports){
+},{"../generators/noise-generators.js":2,"../utilities/dom-utilities.js":6,"p5-bbox-aligned-text":1}],5:[function(require,module,exports){
 module.exports = startSketch;
 
 // Modules
@@ -249,8 +526,8 @@ var canvasSize = {
 	width: 400,
 	height: 150
 };
-var text = "String";
-var fontSize = 120;
+var text = "Squiggle";
+var fontSize = 100;
 var fontPath = "./assets/fonts/leaguegothic-regular-webfont.ttf";
 
 function startSketch() {	
@@ -323,7 +600,7 @@ function draw() {
 		p.text(text, 0, 0);
 	p.pop();
 }
-},{"../generators/noise-generators.js":1,"../utilities/dom-utilities.js":5}],5:[function(require,module,exports){
+},{"../generators/noise-generators.js":2,"../utilities/dom-utilities.js":6}],6:[function(require,module,exports){
 module.exports.forEachInObject = function (object, iterationFunction) {
 	if (!object) return;
 	for (var key in object) {
@@ -362,7 +639,7 @@ module.exports.addAttributes = function (element, attributes) {
 module.exports.removeElement = function (element) {
 	element.parentElement.removeChild(element);
 };
-},{}]},{},[2])
+},{}]},{},[3])
 
 
 //# sourceMappingURL=main/main.js.map
