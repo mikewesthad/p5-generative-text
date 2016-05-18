eaad8803e01948f8676e07017b66d1899e164ad4
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-17 22:57:03 -0500

    Gulp fix - browserify errors won't break gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,10 @@ gulp.task("js", function() {
         debug: true
     })
     return b.bundle()
-        .on("error", gutil.log)
+        .on("error", function (err) {
+            gutil.log(err);
+            this.emit("end");
+        })
         .pipe(source("main.js"))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
