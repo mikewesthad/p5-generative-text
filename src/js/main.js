c017b6f86673dd5497bad82bbf9605b7cec6f9f1
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-18 21:52:01 -0500

    Load new sketch in main

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 require("./sketches/noisy-word.js")();
 require("./sketches/halftone-flashlight-word.js")();
 require("./sketches/word-particle-wrapping.js")();
+require("./sketches/fleeing-halftones.js")();
